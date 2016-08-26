a48147844ff5a79c92db3b70e86932424e7fecb1
swiecone <alejandro.swiec@gmail.com>
2016-08-26 20:56:15 +0200

    Created a folder for assessment

--- Course 1 - HTML, CSS and JavaScript/Week 2 (Introduction to JavaSript)/Week 2 Assessment/js_gcolor_guess_game.js
+++ Course 1 - HTML, CSS and JavaScript/Week 2 (Introduction to JavaSript)/Week 2 Assessment/js_gcolor_guess_game.js
@@ -3,9 +3,10 @@ var guess_input_color;
 var guess_input;
 var finished = false;
 var guesses = 0;
+var color_array = ["Black", "Blue", "Brown", "Cyan", "DarkOrange", "DeepPink", "Gold", "Olive", "Pink", "White", "Yellow"];
+
 
 function do_game() {
-	var color_array = ["Black", "Blue", "Brown", "Cyan", "DarkOrange", "DeepPink", "Gold", "Olive", "Pink", "White", "Yellow"];
 	//alert("This are all the colors:" + color_array);
 	var random_color = Math.random() * color_array.length;
 	var random_color_integer = Math.floor(random_color);
@@ -14,9 +15,6 @@ function do_game() {
 	//this is to present the colors in the prompt
 	alert("The color answer is: "+ target);
 	
-	guess_input_color = prompt("I am thinking in one of the following colors:\n\n "+ color_array+"\n\nWhich color am I thinking of? ");
-	console.log(guess_input_color);
-
 
 	while (!finished) {
 		guess_input_color = prompt("I am thinking in one of the following colors:\n\n "+ color_array+"\n\nWhich color am I thinking of? ");
@@ -36,18 +34,29 @@ function check_guess() {
 	// 	alert("Please enter an integer number in the range of 1 to 100.");
 	// 	return false;
 	// }
+	var index = color_array.indexOf(guess_input_color);
+
+    if (index < 0) {
+        alert("I don’t recognize that color!");
+        return false;
+    }
+
+
 	if (guess_input_color.localeCompare(target) > 0) {
 		alert("Your guess color higher in alphabetical order!");
 		return false;
 	}
+
 	if (guess_input_color.localeCompare(target) < 0) {
 		alert("our guess color lower in alphabetical order!");
 		return false;
 	}
-	alert("You got it! The color is "+target+
-		".\n\nIt took you "+guesses+
-		" to get the number.");
-	return true;
+		// document.body.style.backgroundColor = target;
+		
+		 elem_body = document.getElementsByTagName("body")[0];
+ 		 elem_body.style.background = target;
+		alert("You got it! The color is "+target+ ".\n\nIt took you "+guesses+" to get the number.");
+        return true;
 	}
 }
 
