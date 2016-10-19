aa07ad36d5bc80fc87ff4b5d66a62dc268caef48
swiecone <alejandro.swiec@gmail.com>
2016-10-19 23:28:58 +0200

    configured gruntjs file

--- Course 3 -  FE JS Framework - AngularJS/ConFusion/Gruntfile.js
+++ Course 3 -  FE JS Framework - AngularJS/ConFusion/Gruntfile.js
@@ -3,6 +3,31 @@
 module.exports = function(grunt) {
   // Define the configuration for all the tasks
   grunt.initConfigP({
+    //Time how long taks take.  Can help when optimizing build items
+    require('time-grunt')(grunt);
 
+    //Automatically load required Grunt tasks
+    require('jit-grunt')(grunt);
+
+    //Define the configuration for all the tasks
+    grunt.initConfig({
+
+      pkg: grunt.file.readJSON('package.json'),
+
+      //Make sure code syles are up to par and there are no obvious mistakes
+        jshint: {
+          options: {
+            jshintrc: '.jshintrc',
+            reporter: require('jshint-stylish')
+          },
+          all: {
+            src: [
+              'Gruntfile.js',
+              'app/scripts/{,*/}*.js'
+            ]
+
+          }
+        }
+    });
   });
 }
