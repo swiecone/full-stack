2dead8ac8ea46b5608c65c2230d16d34ea88a951
swiecone <alejandro.swiec@gmail.com>
2016-11-06 14:47:49 +0100

    Added a data structure to update the number of stars selected for the dish detail page.

--- Course 3 -  FE JS Framework - AngularJS/Assignments- ConFusion/app/scripts/app.js
+++ Course 3 -  FE JS Framework - AngularJS/Assignments- ConFusion/app/scripts/app.js
@@ -162,6 +162,12 @@ angular.module('confusionApp', [])
                     };
 
 
+            var starts = [{value:"1", label:"1"},
+                          {value:"2", label:"2"},
+                          {value:"3", label:"3"},
+                          {value:"4", label:"4"},
+                          {value:"5", label:"5"}]
+
             $scope.commentSearch = function(val)
                     {
                       $scope.searchText = val;
