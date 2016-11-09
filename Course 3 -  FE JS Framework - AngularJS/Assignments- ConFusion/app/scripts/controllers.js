913564322b017668d8e26aa39295d08d7521f491
swiecone <alejandro.swiec@gmail.com>
2016-11-09 12:55:25 +0100

    Added changes so that dish detail could also be shown

--- Course 3 -  FE JS Framework - AngularJS/Assignments- ConFusion/app/scripts/controllers.js
+++ Course 3 -  FE JS Framework - AngularJS/Assignments- ConFusion/app/scripts/controllers.js
@@ -3,7 +3,6 @@
 angular.module('confusionApp')
 
  .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
-
     $scope.tab = 1;
     $scope.filtText = '';
     $scope.showDetails = true;
@@ -63,10 +62,51 @@ angular.module('confusionApp')
     
   }])
 
-.controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+  .controller('DetailController', ['$scope', function($scope) {
+
             $scope.searchText = '';
 
-            $scope.dish= menuFactory.getDish(3);
+            $scope.dish={
+                          name:'Uthapizza',
+                          image: 'images/uthapizza.png',
+                          category: 'mains',
+                          label:'Hot',
+                          price:'3.99',
+                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                           comments: [
+                               {
+                                   rating:5,
+                                   comment:"Imagine all the eatables, living in conFusion!",
+                                   author:"John Lemon",
+                                   date:"2012-10-16T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                                   author:"Paul McVites",
+                                   date:"2014-09-05T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:3,
+                                   comment:"Eat it, just eat it!",
+                                   author:"Michael Jaikishan",
+                                   date:"2015-02-13T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Ultimate, Reaching for the stars!",
+                                   author:"Ringo Starry",
+                                   date:"2013-12-02T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:2,
+                                   comment:"It's your birthday, we're gonna party!",
+                                   author:"25 Cent",
+                                   date:"2011-12-02T17:57:28.556094Z"
+                               }
+
+                           ]
+                    };
 
          
 
@@ -76,9 +116,7 @@ angular.module('confusionApp')
                     }
 
         var now = Date();
-        var newdate = now.getDate();
         console.log(now);
-        console.log(newdate);
 
             $scope.newComment = {author:" ", rating:"5",
                        comment:"",  date:""};
