6df1296041564d672ccd9fa7405575e9bd6c1591
swiecone <alejandro.swiec@gmail.com>
2016-11-14 23:13:24 +0100

    minor changes to services.js

--- Course 3 -  FE JS Framework - AngularJS/Assignments- ConFusion/app/scripts/services.js
+++ Course 3 -  FE JS Framework - AngularJS/Assignments- ConFusion/app/scripts/services.js
@@ -3,7 +3,6 @@
 
 angular.module('confusionApp')
         .constant("baseURL", "http://localhost:3000/")
-        
         .service('menuFactory', ['$resource','baseURL',
     function($resource,baseURL) {
 
