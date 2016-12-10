7d8204b4429534c27da82f10a1751c9657bac14e
swiecone <alejandro.swiec@gmail.com>
2016-12-11 00:04:00 +0100

    fixed the menu, put a list and added left image as thumbnail

--- Course 4 - Hybrid Mobile App Dev/ionic/conFusion/www/js/controllers.js
+++ Course 4 - Hybrid Mobile App Dev/ionic/conFusion/www/js/controllers.js
@@ -41,8 +41,9 @@ angular.module('conFusion.controllers', [])
   };
 })
 
-    .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+    .controller('MenuController', ['$scope', 'menuFactory', 'baseURL', function($scope, menuFactory, baseURL) {
             
+            $scope.baseURL = baseURL;
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
@@ -127,8 +128,9 @@ angular.module('conFusion.controllers', [])
             };
         }])
 
-        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', 'baseURL',function($scope, $stateParams, menuFactory, baseURL) {
 
+             $scope.baseURL = baseURL;
              $scope.showDish = true;
              $scope.newComment = {author:"", rating:"5",
                        comment:"",  date:""};
